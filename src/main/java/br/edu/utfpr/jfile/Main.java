5d3793640c1a0412f780f887d94a10cbd8ddac43
Matheus Dias <matheusdyas007@hotmail.com>
2022-05-03 17:35:02 -0300

    feat: implementado alguns menus

--- src/main/java/br/edu/utfpr/jfile/Main.java
+++ src/main/java/br/edu/utfpr/jfile/Main.java
@@ -12,11 +12,11 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1019, 617);
-        stage.setTitle("JFile!");
+        Scene scene = new Scene(fxmlLoader.load(), 1150, 771);
+        stage.setTitle("JFile");
         stage.setScene(scene);
         stage.resizableProperty().setValue(Boolean.FALSE);
-        stage.getIcons().add(new Image("file:C:\\Users\\aluno\\IdeaProjects\\JFile\\src\\main\\resources\\br\\edu\\utfpr\\imgs\\icone_pasta_30.png"));
+        stage.getIcons().add(new Image("file:src/main/resources/br/edu/utfpr/imgs/icone_pasta_30.png"));
         stage.show();
     }
 
