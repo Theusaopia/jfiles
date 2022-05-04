0ae2b3a12bae618403b18db7862b6236a5466d9a
matheus <mdias98@outlook.com>
2022-05-03 21:41:55 -0300

    feat: teste

--- src/main/java/br/edu/utfpr/jfile/Main.java
+++ src/main/java/br/edu/utfpr/jfile/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1150, 771);
-        stage.setTitle("JFile - Arquivos");
+        stage.setTitle("JFile");
         stage.setScene(scene);
         stage.resizableProperty().setValue(Boolean.FALSE);
         stage.getIcons().add(new Image("file:src/main/resources/br/edu/utfpr/imgs/icone_pasta_30.png"));
