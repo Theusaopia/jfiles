45fe93908d032da670aabf77002a6208f55aeef9
Matheus Dias <matheusdyas007@hotmail.com>
2022-05-10 22:21:02 -0300

    feat: criado botoes para upload de arquivos de acordo com a pagina

--- src/main/java/br/edu/utfpr/jfile/PageAudio.java
+++ src/main/java/br/edu/utfpr/jfile/PageAudio.java
@@ -1,4 +1,17 @@
 package br.edu.utfpr.jfile;
 
+import javafx.event.ActionEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class PageAudio {
+    public void actionUploadFile(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setTitle("Escolha um aúdio");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Arquivos de aúdio", "*.mp3"));
+
+        File selectedFile = fileChooser.showOpenDialog(null);
+    }
 }
--- src/main/java/br/edu/utfpr/jfile/PageVideo.java
+++ src/main/java/br/edu/utfpr/jfile/PageVideo.java
@@ -1,4 +1,17 @@
 package br.edu.utfpr.jfile;
 
+import javafx.event.ActionEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class PageVideo {
+    public void actionUploadFile(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setTitle("Escolha um vídeo");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Arquivos de vídeo", "*.mp4"));
+
+        File selectedFile = fileChooser.showOpenDialog(null);
+    }
 }
