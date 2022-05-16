33e299c49dadeba5c341a063a7e040c134caece1
Matheus Dias <matheusdyas007@hotmail.com>
2022-05-16 10:58:28 -0300

    feat: criado gerenciador de alertas

--- src/main/java/br/edu/utfpr/jfile/util/AlertMaker.java
+++ src/main/java/br/edu/utfpr/jfile/util/AlertMaker.java
@@ -2,7 +2,7 @@ package br.edu.utfpr.jfile.util;
 
 import javafx.scene.control.Alert;
 
-public abstract class AlertBuilder {
+public abstract class AlertMaker {
     public static Alert generateNewAlert(Alert.AlertType typeOfAlert, String contentText, String title) {
         Alert a = new Alert(typeOfAlert);
         a.setContentText(contentText);
